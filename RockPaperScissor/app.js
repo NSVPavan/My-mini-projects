8849f0a93ed55c32730579e449f0e8c5c76f6ec4
Naramala siva venkata pavan <sivavenkatapavan@gmail.com>
2024-06-25 01:18:49 +0530

    Add call for restart game when prompt is cancelled

--- RockPaperScissor/app.js
+++ RockPaperScissor/app.js
@@ -1,12 +1,12 @@
 let gameStarted=false;
 let responses=['ROCK','PAPER','SCISSOR'];
-let h3=document.querySelector('h3');
 function randomResponse(){
     let response=Math.floor(Math.random()*3)+0;
     return responses[response];
 }
 function restartGame(){
     gameStarted=false;
+    let h3=document.querySelector('h3');
     h3.innerText='Press any key to start!';
     h3.style.color='red';
 }
@@ -16,16 +16,23 @@ document.addEventListener('keydown',()=>{
     }
     gameStarted=true;
     console.log('Game has started!');
+    let h3=document.querySelector('h3');
     h3.innerText='Game has started, head to console.';
     h3.style.color='green';
     for(let round=1;round<=5;round++){
         console.log(`Round-${round}`);
         let userResponse="";
-        while(!responses.includes(userResponse.toUpperCase())){
-            userResponse=prompt("What's your move, rock, paper or scissor?");
-            if(!responses.includes(userResponse.toUpperCase())){
-                console.error("Enter a valid move!");
+        try{
+            while(!responses.includes(userResponse.toUpperCase())){
+                userResponse=prompt("What's your move, rock, paper or scissor?");
+                if(!responses.includes(userResponse.toUpperCase())){
+                    console.error("Enter a valid move!");
+                }
             }
+        }catch(error){
+            console.warn('Some error occured. The game has restarted.');
+            restartGame();
+            return;
         }
     }
 })
