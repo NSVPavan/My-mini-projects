567da47eeeb5f0ec8130f9cee71cee7aa1783f19
Naramala siva venkata pavan <sivavenkatapavan@gmail.com>
2024-06-25 00:36:56 +0530

    Add start game on pressing any key functionality

--- RockPaperScissor/app.js
+++ RockPaperScissor/app.js
@@ -1,1 +1,11 @@
-console.log("Connection successful");
+let gameStarted=false;
+document.addEventListener('keydown',()=>{
+    if(gameStarted){
+        return;
+    }
+    gameStarted=true;
+    console.log('Game has started!');
+    let h3=document.querySelector('h3');
+    h3.innerText='Game has started, head to console.';
+    h3.style.color='green';
+})
