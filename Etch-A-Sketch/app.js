229440d21ed52c196b4ea4aa7bbcdf2eb894af26
Naramala siva venkata pavan <sivavenkatapavan@gmail.com>
2024-07-05 18:28:10 +0530

    Add cell color change on hover for Etch-A-Sketch

--- Etch-A-Sketch/app.js
+++ Etch-A-Sketch/app.js
@@ -16,11 +16,12 @@ button.addEventListener('click',()=>{
     n=input.value;
     for(let i=0;i<n*n;i++){
         let cell = document.createElement('div');
+        cell.addEventListener('mouseenter',()=>{
+            cell.style.backgroundColor='black';
+        });
         cell.classList.add('cell');
         cell.style.height=`${700/parseFloat(n)-2}px`; //To account for borders
         cell.style.width=`${700/parseFloat(n)-2}px`;
         grid.appendChild(cell);
-        console.log('ok');
     }
-    console.log('added');
 });
